d1d6d8b5b7655f483894f1dd91052bcb1f6b0554
Spandan Pagar <spandanpagar2002@gmail.com>
2025-11-29 18:23:55 +0530

    Requirements added and setup created.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Spandan",
+    author_email="spandanpagar2002@gmail.com",
+    packages=find_packages()
+)
